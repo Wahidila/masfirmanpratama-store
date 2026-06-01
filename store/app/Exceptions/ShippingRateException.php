01e4ba50c7c2b3ca155947dc8dc1e5ea3a44939b
root <root@localhost.localdomain>
2026-06-01 20:55:03 +0800

    feat(shipping): add ShippingRateException (ship-errfix-a)

---
+++ store/app/Exceptions/ShippingRateException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Exception untuk error operasional shipping API (license expired, koneksi gagal, dll).
+ * 
+ * Dibedakan dari Throwable umum supaya controller bisa handle spesifik:
+ * - Logging sudah dilakukan di layer bawah (AgenwebsiteClient)
+ * - Controller convert ke pesan user-friendly
+ * - No-coverage asli (success tapi empty) TIDAK throw ini
+ */
+class ShippingRateException extends RuntimeException
+{
+    //
+}
